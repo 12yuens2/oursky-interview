5d8b8ea58bc2ea9c155b48fbe479383a3f042b64
dR3am5t0rM <sizhe1007@gmail.com>
2017-03-29 10:47:52 +0100

    Added pausing, stopping and displaying the time properly.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var SECOND = 1000;
 var TIMERS = [];
 
+var SECONDS_IN_HOUR = 3600;
+var SECONDS_IN_MINUTE = 60;
 
 function add_new_task() {
 	
@@ -8,11 +10,24 @@ function add_new_task() {
 	var name = document.getElementById("name").value;
 	document.getElementById("name").value = "";
 
-	var task_element = document.createElement("p");
+	/* Create new task */
+	var task_element = create_task_element();
 	var task = new Task(name, task_element);
-	task.updateElement();
+	task.update_element();
 
+	/* Append task to HTML */
 	$("body").append(task_element);
+}
+
+function create_task_element() {
+	var div = $("<div>", {"class": "task"});
+
+	$(div).append($("<span class='name'></span>"));
+	$(div).append($("<button class='stop'>s</button>"));
+	$(div).append($("<button class='pause'>p</button>"));
+	$(div).append($("<span class='time'></span>"));
+
+	return div;
 }
 
 var Task = function(name, element) {
@@ -20,14 +35,66 @@ var Task = function(name, element) {
 	this.element = element;
 	this.time = 0;
 
+	this.name_element = $(this.element).find(".name");
+	this.time_element = $(this.element).find(".time");
+	this.pause_button = $(this.element).find(".pause");
+	this.stop_button = $(this.element).find(".stop");
+
+	this.tick = function() {
+		this.time++
+		this.update_element();
+	}
+
 	/* Increment time for this task every second */
-	this.timer = setInterval(function() {
-		this.time++;
-		this.updateElement();
-	}.bind(this), SECOND);
+	this.timer = setInterval(this.tick.bind(this), SECOND);
+
+	/* Variable for dealing with scope */
+	var task = this;
+
+	/* Handler for pausing and resuming */
+	this.pause_button.click(function() {
+		/* Pause the timer */
+		if (task.timer) {
+			$(this).html("r");
+			clearInterval(task.timer)
+			task.timer = undefined;
+		} 
+
+		/* Resume the timer */
+		else {
+			$(this).html("p");
+			task.timer = setInterval(task.tick.bind(task), SECOND);
+		}
+	});
+
+	/* Handler for stopping */
+	this.stop_button.click(function() {
+		clearInterval(task.timer);
+
+		/* Disable buttons */
+		$(task.pause_button).prop("disabled", true);
+		$(task.stop_button).prop("disabled", true);
+	});
 
 	/* Update HTML element */
-	this.updateElement = function() {
-		this.element.innerHTML = this.name + " " + this.time;
+	this.update_element = function() {
+		$(this.name_element).html(this.name);
+		$(this.time_element).html(this.display_time());
+	}
+
+	/* Display the time in hrs:mins:secs */
+	this.display_time = function() {
+		var time = this.time
+		var hours = Math.floor(time / SECONDS_IN_HOUR);
+		hours = hours == 0 ? "" : hours + " hrs ";
+		time -= hours * SECONDS_IN_HOUR;
+
+		var minutes = Math.floor(time / SECONDS_IN_MINUTE);
+		minutes = minutes == 0 ? "" : minutes + " mins ";
+		time -= minutes * SECONDS_IN_MINUTE;
+
+		var seconds = time + " secs";
+
+		return hours + minutes + seconds;
 	}
 }
